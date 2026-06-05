8b5d8b1c36c6894344b5c1c90d07e5038661dbb8
Mythreyahari <mythreyan53@gmail.com>
2026-06-05 11:01:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+// const cors = require("cors")
+
+const app = express();
+const PORT = process.env.PORT = 3000
+// app.use(cors)
+
+
+app.use(express.json())
+
+
+app.listen(PORT,()=>{
+    console.log(`server is now running on port:${PORT}`);
+    
+})
+
+
